27a7b26396177bd34712bec234d77c2d94234c73
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-05-29 00:29:17 +0300

    all promises - another try

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const promises = Promise.all([uploadPhoto(), createUser()]);
-  promises.then((responses) => {
-    const [photo, user] = responses;
-    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  })
-    .catch(() => console.log('Signup system offline'));
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
 }
