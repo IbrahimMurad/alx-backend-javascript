a220983a97034970f5cecdef60a2995f7dd58193
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-05-29 00:49:59 +0300

    all promises - another try

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  if (photo.body === undefined || user.firstName === undefined || user.lastName === undefined) {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
     console.log('Signup system offline');
-  } else {
-    await Promise.all([photo, user]).then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-    });
   }
 }
