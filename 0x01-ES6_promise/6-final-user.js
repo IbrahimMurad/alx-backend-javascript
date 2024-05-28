b1b240fd117a8c40b05b13db6dbb9e1eb2409001
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-05-29 01:19:44 +0300

    all settled - one by one?

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  let promisesSettled = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    promisesSettled = [{ status: 'fulfilled', value: user }];
-  } catch (error) {
-    promisesSettled = [{ status: 'rejected', value: error }];
-  }
-  try {
-    const photo = await uploadPhoto(fileName);
-    promisesSettled.push({ status: 'fulfilled', value: photo });
-  } catch (error) {
-    promisesSettled.push({ status: 'rejected', value: error });
-  }
-  return promisesSettled;
+  const promisesSettled = [];
+  const user = signUpUser(firstName, lastName);
+  user
+    .then((data) => promisesSettled.push({ status: 'fulfilled', value: data }))
+    .catch((error) => promisesSettled.push({ status: 'rejected', value: error }));
+  const photo = uploadPhoto(fileName);
+  photo
+    .then((data) => promisesSettled.push({ status: 'fulfilled', value: data }))
+    .catch((error) => promisesSettled.push({ status: 'rejected', value: error }));
+  return new Promise((resolve) => resolve(promisesSettled));
 }
