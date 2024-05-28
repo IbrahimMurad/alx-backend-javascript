9b2dae56695eb4c5508c32d16dac1a3a91852b1c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-05-28 23:54:09 +0300

    all promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = Promise.all([uploadPhoto(), createUser()]);
-
-  promises
+  Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
       console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`);
     })
