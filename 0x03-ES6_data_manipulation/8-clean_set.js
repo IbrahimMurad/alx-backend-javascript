2ed48ef3ea7451935a73df5f89b26ff1221fbbc9
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-06-04 01:19:52 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!(typeof startString !== 'string') || !startString) {
     return '';
   }
   const strings = [];
   set.forEach((string) => {
-    if (string.startsWith(startString)) {
+    if ((typeof string === 'string') && (string.startsWith(startString))) {
       strings.push(string.substring(startString.length));
     }
   });
