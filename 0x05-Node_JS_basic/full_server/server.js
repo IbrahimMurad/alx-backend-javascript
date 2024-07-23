55421c698457ce4f3f1c456128c1658836aeb2f0
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-23 21:07:45 +0300

    fix

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import router from './routes/index';
 
-const http = require('http');
-
 const app = express();
 
 app.user('/', router);
 
-const server = http.createServer(app);
-
-server.listen(1245);
+app.listen(1245);
