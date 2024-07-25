ff9dc6dd279be6f0fe93dd0aeb4d998263901339
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-07-25 13:35:17 +0300

    stub and spy

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call calculateNumber', () => {
+    const calculateNumber = sinon.stub(Utils, 'calculateNumber');
+    calculateNumber.returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+    calculateNumber.restore();
+    consoleSpy.restore();
+  });
+});
